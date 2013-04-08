1a885e4e7d3f6879fc0bb3f2e399acda4932461e
Samer Masterson <nosefrog@gmail.com>
2013-04-08 10:43:33 -0400

    Merged changes

---
+++ README.markdown
@@ -0,0 +1,24 @@
+Snake
+=====
+
+About
+-----
+This is a Snake game written in Python and Pygame.
+
+Dependencies
+------------
+
+  * Pygame  (``sudo apt-get install python-pygame`` on Debian/Ubuntu)
+
+To install Pygame for Python 3, you should to download this [experimental version](https://launchpad.net/debian/experimental/+source/pygame/1.9.2~pre~r3144-1/+files/pygame_1.9.2~pre~r3144.orig.tar.gz) and install the dependencies for Pygame:
+
+  ``sudo apt-get install python3-dev libsdl-image1.2-dev libsdl-mixer1.2-dev  libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion  libportmidi-dev``
+
+Then go to the folder that you extracted for Pygame and run ``sudo python3 setup.py install``.
+
+Usage
+-----
+
+Simply run ``python game.py`` (or ``python3 game.py`` if you're using Python 3).
+
+That wasn't too hard, was it? :)
--- game.py
+++ game.py
@@ -1,6 +1,4 @@
-import sys
-import collections
-import time
+from collections import deque, namedtuple
 import random
 import pygame
 
@@ -10,48 +8,58 @@ OFFSET = 16
 class Directions:
     Up, Down, Left, Right = range(4)
 
-def findFood(spots):
+DIRECTIONS = namedtuple('DIRECTIONS',
+                        ['Up', 'Down', 'Left', 'Right'])(0, 1, 2, 3)
+
+
+def find_food(spots):
     while True:
         food = random.randrange(BOARD_LENGTH), random.randrange(BOARD_LENGTH)
-        if (not (spots[food[0]][food[1]] == 1 or spots[food[0]][food[1]] == 2)):
+        if (not (spots[food[0]][food[1]] == 1 or
+                 spots[food[0]][food[1]] == 2)):
             break
     return food
-    
-def endCondition(board, coord):
-    if (coord[0] < 0 or coord[0] >= BOARD_LENGTH or coord[1] < 0 or coord[1] >= BOARD_LENGTH):
+
+
+def end_condition(board, coord):
+    if (coord[0] < 0 or coord[0] >= BOARD_LENGTH or coord[1] < 0 or
+            coord[1] >= BOARD_LENGTH):
         return True
-    if (board[coord[0]][coord[1]] == 1): 
+    if (board[coord[0]][coord[1]] == 1):
         return True
     return False
 
-def updateBoard(screen, snake, food):
+
+def update_board(screen, snake, food):
     white = (255, 255, 255)
     black = (0, 0, 0)
 
-    rect = pygame.Rect(0,0,OFFSET,OFFSET)
-    
+    rect = pygame.Rect(0, 0, OFFSET, OFFSET)
+
     spots = [[] for i in range(BOARD_LENGTH)]
     num1 = 0
     num2 = 0
     for row in spots:
         for i in range(BOARD_LENGTH):
             row.append(0)
-            temprect = rect.move(num1*OFFSET,num2*OFFSET) 
+            temprect = rect.move(num1 * OFFSET, num2 * OFFSET)
             pygame.draw.rect(screen, white, temprect)
             num2 += 1
         num1 += 1
     spots[food[0]][food[1]] = 2
-    temprect = rect.move(food[1]*OFFSET, food[0]*OFFSET)
+    temprect = rect.move(food[1] * OFFSET, food[0] * OFFSET)
     pygame.draw.rect(screen, black, temprect)
     for coord in snake:
         spots[coord[0]][coord[1]] = 1
-        temprect = rect.move(coord[1]*OFFSET, coord[0]*OFFSET)
+        temprect = rect.move(coord[1] * OFFSET, coord[0] * OFFSET)
         pygame.draw.rect(screen, black, temprect)
     return spots
 
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode([BOARD_LENGTH*OFFSET, BOARD_LENGTH*OFFSET])
+    screen = pygame.display.set_mode([BOARD_LENGTH * OFFSET,
+                                      BOARD_LENGTH * OFFSET])
     pygame.display.set_caption("Snaake")
     clock = pygame.time.Clock()
 
@@ -59,18 +67,18 @@ def main():
     for row in spots:
         for i in range(BOARD_LENGTH):
             row.append(0)
-    
+
     # Board set up
     tailmax = 4
-    direction = Directions.Right
-    snake = collections.deque()
-    snake.append((0,0))
+    direction = DIRECTIONS.Right
+    snake = deque()
+    snake.append((0, 0))
     spots[0][0] = 1
-    food = findFood(spots)
+    food = find_food(spots)
     spots[food[0]][food[1]] = 2
 
     while True:
-        # Event processing 
+        # Event processing
         done = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -78,17 +86,17 @@ def main():
                 done = True
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    if direction != Directions.Down:
-                         direction = Directions.Up
+                    if direction != DIRECTIONS.Down:
+                         direction = DIRECTIONS.Up
                 elif event.key == pygame.K_DOWN:
-                    if direction != Directions.Up:
-                         direction = Directions.Down
+                    if direction != DIRECTIONS.Up:
+                         direction = DIRECTIONS.Down
                 elif event.key == pygame.K_RIGHT:
-                    if direction != Directions.Left:
-                         direction = Directions.Right
+                    if direction != DIRECTIONS.Left:
+                         direction = DIRECTIONS.Right
                 elif event.key == pygame.K_LEFT:
-                    if direction != Directions.Right:
-                         direction = Directions.Left
+                    if direction != DIRECTIONS.Right:
+                         direction = DIRECTIONS.Left
 
 
         if done:
@@ -96,36 +104,35 @@ def main():
 
         # Game logic
         head = snake.pop()
-        if (direction == Directions.Up):
-            nextHead = (head[0] - 1, head[1])
-        elif (direction == Directions.Down):
-            nextHead = (head[0] + 1, head[1])
-        elif (direction == Directions.Left):
-            nextHead = (head[0], head[1] - 1)
-        elif (direction == Directions.Right):
-            nextHead = (head[0], head[1] + 1)
-        if (endCondition(spots, nextHead)):
-            print(nextHead)
+        if (direction == DIRECTIONS.Up):
+            next_head = (head[0] - 1, head[1])
+        elif (direction == DIRECTIONS.Down):
+            next_head = (head[0] + 1, head[1])
+        elif (direction == DIRECTIONS.Left):
+            next_head = (head[0], head[1] - 1)
+        elif (direction == DIRECTIONS.Right):
+            next_head = (head[0], head[1] + 1)
+        if (end_condition(spots, next_head)):
+            print(next_head)
             print("end condition reached")
             break
 
-        if spots[nextHead[0]][nextHead[1]] == 2:
+        if spots[next_head[0]][next_head[1]] == 2:
             tailmax += 4
-            food = findFood(spots)
+            food = find_food(spots)
 
         snake.append(head)
-        snake.append(nextHead)
-        
+        snake.append(next_head)
+
         if len(snake) > tailmax:
             tail = snake.popleft()
 
         # Draw code
-        black = (0,0,0)
+        black = (0, 0, 0)
         white = (255, 255, 255)
-        screen.fill(white) # makes screen white
-        
-        spots = updateBoard(screen, snake, food)
+        screen.fill(white)  # makes screen white
 
+        spots = update_board(screen, snake, food)
 
 #        pygame.draw.line(screen, white, (60, 60), (120, 60), 4)
         pygame.display.update()
