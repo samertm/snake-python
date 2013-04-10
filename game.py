05ef28b0d4c4e85c5740a991885cf802a635e9b9
Samer Masterson <nosefrog@gmail.com>
2013-04-10 17:16:15 -0400

    Added a game over screen, changed the main function

--- game.py
+++ game.py
@@ -9,14 +9,14 @@ BLACK = (0, 0, 0)
 
 
 DIRECTIONS = namedtuple('DIRECTIONS',
-                        ['Up', 'Down', 'Left', 'Right'])(0, 1, 2, 3)
+        ['Up', 'Down', 'Left', 'Right'])(0, 1, 2, 3)
 
 
 def find_food(spots):
     while True:
         food = random.randrange(BOARD_LENGTH), random.randrange(BOARD_LENGTH)
         if (not (spots[food[0]][food[1]] == 1 or
-                 spots[food[0]][food[1]] == 2)):
+            spots[food[0]][food[1]] == 2)):
             break
     return food
 
@@ -55,12 +55,34 @@ def update_board(screen, snake, food):
 def rand_color():
     return (random.randrange(254), random.randrange(254), random.randrange(254))
 
+# Return 0 to exit the program, 1 for a one-player game
+def menu(screen):
+    menu_message = pygame.font.Font(None, 30).render("Press enter to start", True, BLACK)
+    screen.fill(WHITE)
+    screen.blit(menu_message, (32, 32)) 
+    pygame.display.update()
+    while True: 
+        done = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Quit given")
+                done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    entered = True
+                    return 1
+        if done:
+            break
+    if done:
+        pygame.quit()
+        return 0
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode([BOARD_LENGTH * OFFSET,
-                                      BOARD_LENGTH * OFFSET])
-    pygame.display.set_caption("Snaake")
+def quit(screen):
+    return False
+
+# Return false to quit program, true to go to
+# gameover screen
+def oneplayer(screen): 
     clock = pygame.time.Clock()
 
     spots = [[] for i in range(BOARD_LENGTH)]
@@ -75,28 +97,6 @@ def main():
     snake.append((0, 0, rand_color()))
     spots[0][0] = 1
     food = find_food(spots)
-    spots[food[0]][food[1]] = 2
-
-    # Menu screen
-    menu_message = pygame.font.Font(None, 30).render("Press enter to start", True, BLACK)
-    screen.fill(WHITE)
-    screen.blit(menu_message, (32, 32)) 
-    pygame.display.update()
-    entered = False
-    while not entered:
-        done = False
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                print("Quit given")
-                done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    entered = True
-        if done:
-            break
-    if done:
-        pygame.quit()
-        return
 
     while True:
         clock.tick(15)
@@ -109,20 +109,20 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     if direction != DIRECTIONS.Down:
-                         direction = DIRECTIONS.Up
+                        direction = DIRECTIONS.Up
                 elif event.key == pygame.K_DOWN:
                     if direction != DIRECTIONS.Up:
-                         direction = DIRECTIONS.Down
+                        direction = DIRECTIONS.Down
                 elif event.key == pygame.K_RIGHT:
                     if direction != DIRECTIONS.Left:
-                         direction = DIRECTIONS.Right
+                        direction = DIRECTIONS.Right
                 elif event.key == pygame.K_LEFT:
                     if direction != DIRECTIONS.Right:
-                         direction = DIRECTIONS.Left
+                        direction = DIRECTIONS.Left
                 break
 
         if done:
-            break
+            return False
 
         # Game logic
         head = snake.pop()
@@ -137,7 +137,7 @@ def main():
         if (end_condition(spots, next_head)):
             print(next_head)
             print("end condition reached")
-            break
+            return tailmax
 
         if spots[next_head[0]][next_head[1]] == 2:
             tailmax += 4
@@ -156,7 +156,50 @@ def main():
 
         pygame.display.update()
 
-    print("foods eaten %d" % (tailmax / 4 - 1))
+def game_over(screen, eaten):
+    message1 = "You ate %d foods" % eaten
+    game_over_message1 = pygame.font.Font(None, 30).render(message1, True, BLACK)
+    message2 = "Press enter to play again, esc to quit."
+    game_over_message2 = pygame.font.Font(None, 30).render(message2, True, BLACK)
+
+    screen.blit(game_over_message1, (32, 32))
+    screen.blit(game_over_message2, (62, 62))
+    pygame.display.update()
+
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+                if event.key == pygame.K_RETURN:
+                    return True
+
+ 
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode([BOARD_LENGTH * OFFSET,
+        BOARD_LENGTH * OFFSET])
+    pygame.display.set_caption("Snaake")
+
+    first = True
+    playing = True
+    while playing:
+        if first:
+            pick = menu(screen)
+
+        options = {0 : quit,
+                1 : oneplayer}
+        now = options[pick](screen)
+        if now == False:
+            break
+        else:
+            eaten = now / 4 - 1
+            playing = game_over(screen, eaten)
+            first = False
 
     pygame.quit()
 
